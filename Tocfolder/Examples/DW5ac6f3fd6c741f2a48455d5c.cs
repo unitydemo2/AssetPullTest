b8d29362abbc3dd98caed401115d898b0392c920
unitydemo2 <unitydemo2@yopmail.com>
2018-05-02 12:14:38 +0530

    DW CMS Message

---
+++ Tocfolder/Examples/DW5ac6f3fd6c741f2a48455d5c.cs
@@ -0,0 +1,19 @@
+class Star 
+{
+public static void main(String[] args) sample programming
+{
+ int i, j, k;
+for(i=5;i>=1;i--)
+{
+for(j=5;j>i;j--)
+{
+System.out.print(" ");
+}
+for(k=1;k<(i*2);k++)
+{
+System.out.print("*");
+}
+System.out.println();
+}
+}
+}
--- Tocfolder/ManualVersions.md
+++ Tocfolder/ManualVersions.md
@@ -1,2 +1,4 @@
 ![](Images/5ac6160e6c741f2a48455310.jpg)
 
+[5ac6f3fd6c741f2a48455d5c](Examples/5ac6f3fd6c741f2a48455d5c.cs)
+
