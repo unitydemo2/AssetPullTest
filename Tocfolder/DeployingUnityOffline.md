6dcf34259c6308da239a364e88ad989acbcf2d0e
CMSQATeam <cmsqa123@gmail.com>
2019-01-07 12:10:41 +0530

    DW CMS Message

--- Tocfolder/DeployingUnityOffline.md
+++ Tocfolder/DeployingUnityOffline.md
@@ -1,3 +1,12 @@
-THIS FILE IS EDITED FROM LOCAL MACHINE
-by 
-Bharathi.
+ 
+ This draft is created for testing the DocWokrs content. 
+ 
+ 
+ ![](Images/test44_5c32eba738ea8a05a48a1166.jpeg) 
+ 
+ [5c32ebda38ea8a05a48a1170](Examples/Initialall drafts history_5c32ebda38ea8a05a48a1170.cs) 
+ 
+ Updated  By, 
+ Bharathi  Paara. 
+ 
+ 
---
+++ Tocfolder/Examples/Initialall drafts history_5c32ebda38ea8a05a48a1170.cs
@@ -0,0 +1,45 @@
+{
+   "content":    {
+      "nodeId": "5a6575694f271c2a20c0ccc8",
+      "drafts": [      {
+         "draftId": "5a6575734f271c2a20c0cccd",
+         "draftName": "New Blank draft-Test1",
+         "draftType": 2,
+         "gDocFileId": "1qrFb4_or2zNWIsVChb_BYktb_fP5-et-_4w1q4oJD7Q",
+         "gDocUrl": "https://docs.google.com/document/d/1qrFb4_or2zNWIsVChb_BYktb_fP5-et-_4w1q4oJD7Q/edit?usp=drivesdk",
+         "latestSnapshotId": "5a6576d74f271c2a20c0ccd6",
+         "nodeId": "5a6575694f271c2a20c0ccc8"
+      }]
+   },
+   "userId": "S-1-5-21-1210654208-2246142303-2829877410-19101",
+   "flowMap":    {
+      "cmsOperation": 11,
+      "eventGroups": [[      {
+         "eventName": 1014,
+         "eventLocationInFlowMap": null,
+         "status": 2,
+         "childCMSOperation": 0,
+         "childOperationStatus": null,
+         "eventCondition": null,
+         "reuseCmsOperation": 0,
+         "conditionKeys": null,
+         "requestMapProperty": null,
+         "responseMapProperty": null
+      }]],
+      "notificationType": 1,
+      "notificationTopic": "NA",
+      "parentResponseId": null,
+      "parentEventLocation": null,
+      "isOperationCompleteWithStatusSuccess": true,
+      "_id": "5a61b2f45f5c1b87d04af66a",
+      "status": 0,
+      "modifiedDate": 0,
+      "createdDate": 1516352244
+   },
+   "createdOn": "2018-01-22T05:30:44.017Z",
+   "errorResponse": null,
+   "_id": "5a6577042c54ee2828953bed",
+   "status": 2,
+   "modifiedDate": 1516599044,
+   "createdDate": 1516599044
+}
