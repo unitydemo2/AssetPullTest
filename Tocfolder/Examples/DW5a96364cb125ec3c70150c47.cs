6bbdca98e867b8d898c5cf03a2db4defb4ed1573
msahu-rythmos <msahu@rythmos.com>
2019-01-10 13:40:55 +0530

    remove extra content

--- Tocfolder/Examples/DW5a96364cb125ec3c70150c47.cs
+++
@@ -1,2 +0,0 @@
-
-using System; using System.Collections.Generic; using System.IO; using System.Net; using CloudinaryDotNet; using CloudinaryDotNet.Actions; using System.Linq; namespace ConsoleApp1 { class Program { static void Main(string[] args) {  Func<string, string, string> trimTocPath = ''
