84ab87dbeccfb281b41d135a4c9388ddd47e5adb
CMSQATeam <cmsqa123@gmail.com>
2019-02-15 17:03:34 +0530

    DW CMS Message

---
+++ Tocfolder/Examples/CSharp Func_5c63aa3ac48b38222c7fbfe1.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+using System.Linq;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func trimTocPath = '';
+            //string str = string.Empty;
+        }
+    }
+}
